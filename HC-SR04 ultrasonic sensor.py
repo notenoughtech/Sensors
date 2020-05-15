9517e3e9368e356f24085e74508b85c6cfb59def
Mat Zolnierczyk <mat@notenoughtech.com>
2020-05-15 20:24:50 +0100

    HC-SR04 added

---
+++ HC-SR04 ultrasonic sensor.py
@@ -0,0 +1,44 @@
+
+STARTER
+
+"Greener than Phil's lawn" Salad - Rabbit Edition
+"Raw Wine" (Grapes)
+"Red Exotic Fruit" (It's strawberries but sounds so much nicer)
+ 
+MAIN
+"Holy Cow!" Burgers:
+  -onion edition
+  -classy edition (for "sophisticated" people)
+
+Hot "Possage"   (Polish sausage)
+
+"Grilled Sea Monsters" 
+	(Everyone's fav prawns - it's what the prawn crackers are made of)
+
+"Charred Chicken Leg"
+	(You wish I was joking witht he "Charred" part)
+
+"Yellow Hen Party" 
+	(Mango and Coconut whatever is left of the uncharred chicken)
+	
+"Strip - Tease" 
+	(cow slices carefully grilled by people who by that time leaned how not to burn the food on BBQ)
+
+ 
+
+DESSERT
+
+(if you still have the space, if you do, you failed at the "MAIN" part of the menu)
+
+"Cheese!"  Say it with a smile or you will get caked!
+"IceCream" - because we are expecting another heatwave
+
+
+Mouth moisturisers
+
+"Fermented barley" (Fresh import from Italy)
+"Posh Bubbles" (French imports)
+"Just add tonic" (Gin based beverage for 18+)
+"Moscat" (why not, it's a posh party)
+"Table White wine" (hey, we have to get smashed on something)
+"Sissy drinks" - (sugar filled alternatives for people who can't handle "posh")
