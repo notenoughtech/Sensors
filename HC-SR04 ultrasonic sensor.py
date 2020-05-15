4d1cf0743c1368ad4b7cdc379b9647467e715353
Mat Zolnierczyk <mat@notenoughtech.com>
2020-05-15 20:31:19 +0100

    accidentally added BBQ menu instead of the python script :)

--- HC-SR04 ultrasonic sensor.py
+++ HC-SR04 ultrasonic sensor.py
@@ -1,44 +1,36 @@
+#required libraries
+import RPi.GPIO as GPIO
+import time
 
-STARTER
+GPIO.setmode(GPIO.BCM) #Set GPIO pin numbering
+TRIG = 2 #Assign PIN to TRIGGER
+ECHO = 21 #Assign PIN to ECHO
 
-"Greener than Phil's lawn" Salad - Rabbit Edition
-"Raw Wine" (Grapes)
-"Red Exotic Fruit" (It's strawberries but sounds so much nicer)
- 
-MAIN
-"Holy Cow!" Burgers:
-  -onion edition
-  -classy edition (for "sophisticated" people)
+print "Checking"
+GPIO.setup(TRIG,GPIO.OUT) #Set pin as GPIO out
+GPIO.setup(ECHO,GPIO.IN) #Set pin as GPIO in
 
-Hot "Possage"   (Polish sausage)
+while True:
+	GPIO.output(TRIG, False) #Set TRIG as LOW
+	print "Waitng For Sensor To Settle"
+	time.sleep(2) #Delay of 2 seconds
 
-"Grilled Sea Monsters" 
-	(Everyone's fav prawns - it's what the prawn crackers are made of)
+	GPIO.output(TRIG, True) #Set TRIG as HIGH
+	time.sleep(0.00001) #Delay of 0.00001 seconds
+	GPIO.output(TRIG, False) #Set TRIG as LOW
 
-"Charred Chicken Leg"
-	(You wish I was joking witht he "Charred" part)
+while GPIO.input(ECHO)==0: #Check whether the ECHO is LOW
+	pulse_start = time.time() #Saves the last known time of LOW pulse
 
-"Yellow Hen Party" 
-	(Mango and Coconut whatever is left of the uncharred chicken)
-	
-"Strip - Tease" 
-	(cow slices carefully grilled by people who by that time leaned how not to burn the food on BBQ)
+while GPIO.input(ECHO)==1: #Check whether the ECHO is HIGH
+	pulse_end = time.time() #Saves the last known time of HIGH pulse
 
- 
+	pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
 
-DESSERT
+	distance = pulse_duration * 17150 #Multiply pulse duration by 17150 to get distance
+	distance = round(distance, 2) #Round to two decimal points
 
-(if you still have the space, if you do, you failed at the "MAIN" part of the menu)
-
-"Cheese!"  Say it with a smile or you will get caked!
-"IceCream" - because we are expecting another heatwave
-
-
-Mouth moisturisers
-
-"Fermented barley" (Fresh import from Italy)
-"Posh Bubbles" (French imports)
-"Just add tonic" (Gin based beverage for 18+)
-"Moscat" (why not, it's a posh party)
-"Table White wine" (hey, we have to get smashed on something)
-"Sissy drinks" - (sugar filled alternatives for people who can't handle "posh")
+	if distance > 2 and distance < 400: #Check whether the distance is within range
+		print "Distance to your object:",distance - 0.5,"cm" #Print distance with 0.5 cm calibration
+	else:
+			print "Out of range" #display out of range
